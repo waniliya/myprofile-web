7e0175b767665c0813663122162523c41f3d1a13
Wan Iliya <70017256+waniliya@users.noreply.github.com>
2024-09-10 00:25:58 +0800

    Update Skill.js

--- src/component/skill/Skill.js
+++ src/component/skill/Skill.js
@@ -11,13 +11,13 @@ const Skill=()=> {
             <h4>Programming Languages</h4>
             <ul>
               <li><b>Advanced: </b> HTML</li>
-              <li><b>Intermediate: </b> CSS, Javascript, PHP, Java/Java EE, C/C++, SQL</li>
-              <li><b>Beginner: </b>Python, React JS, Visual Basic</li></ul>
+              <li><b>Intermediate: </b> CSS, Javascript, PHP, SQL</li>
+              <li><b>Beginner: </b>Python, Java/Java EE, C/C++,</li></ul>
         </div>
         <div className='col-lg-6'>
         <h4>Software/Framework</h4>
         <ul>
-              <li><b>Web/Software Development: </b> React JS, ExpressJS, Eclipse, Jupiter, Matlab</li>
+              <li><b>Web/Software Development: </b> React JS, ExpressJS, Eclipse, Jupiter, Matlab, Dreamweaver</li>
               <li><b>Database: </b>Oracle, MySQL, MongoDB </li>
               <li><b>Graphic Design: </b>Adobe Premiere Pro, After Effect, CapCut, Photoshop, Canva</li>
               <li><b>Game Development: </b>Unity Game Engine, Blender3D</li>
