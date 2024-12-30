ec01d628cf5e0ae4483f975eaaa84e1a612ea57c
Wan Iliya <70017256+waniliya@users.noreply.github.com>
2024-12-30 11:47:14 +0800

    Update Project.js

--- src/component/project/Project.js
+++ src/component/project/Project.js
@@ -2,7 +2,7 @@ import './Project.css'
 import calculator from './../../img/calculatorapp.png';
 import todo from './../../img/todo.png';
 import ecommerce from './../../img/ecommerce.JPG';
-import expense from './../../img/expensesui.jpg';
+import expense from './../../img/expensesui.JPG';
 
 const Project=()=> {
   return (
